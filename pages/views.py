c9232282d4677321452f2320f67cde4bceabf820
Adilson Lima <adilson.monte.alto@gmail.com>
2022-05-13 01:20:30 -0300

    integrando front-end

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import TemplateView
+from trees.models import Square
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['square'] = Square.objects.all() #ordem aleatória
+        return context
+
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+    
+
+
