2ff4a8a1f10f2ac614ea0a5e95b443e113a4e0ed
Adilson Lima <adilson.monte.alto@gmail.com>
2022-05-15 07:55:26 -0300

    Adicionar arquivos para deploy com heroky

--- pysrc/settings/base.py
+++ pysrc/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,13 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = "users.User" # indica que estou usando meu modelo de usuário
 
 # Media
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
--- pysrc/wsgi.py
+++ pysrc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pysrc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pysrc.settings.base')
 
 application = get_wsgi_application()
